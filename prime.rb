4ba0c5bcd93bd3ed2020b2e1316776d549dc1222
wk645 <wk645@nyu.edu>
2017-06-12 17:04:34 -0400

    prime lab

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+
+def prime?(integer)
+    
+    array = (2..integer-1).to_a
+    
+    if integer < 2
+        return false
+    end
+        
+    array.each do |number|
+        if integer % number == 0
+            return false
+            end
+        end
+    return true
+end
